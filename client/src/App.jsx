4d4b0e6913ad43e89dae497999fce340cfca967a
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-05-06 11:04:17 +0700

    ?? Cofig api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,34 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import CssBaseLine from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import AuthLayout from "./components/layout/AuthLayout";
 import Home from "./pages/Home";
 import Board from "./pages/Board";
-import { CssBaseline } from "@mui/material";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 function App() {
   const theme = createTheme({
     palette: { mode: "dark" },
   });
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
+      <CssBaseLine />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
           </Route>
           <Route path="/" element={<AppLayout />}>
             <Route index element={<Home />} />
-            <Route path="/boards" element={<Home />} />
-            <Route path="/boards/:boardId" element={<Board />} />
+            <Route path="boards" element={<Home />} />
+            <Route path="boards/:boardId" element={<Board />} />
           </Route>
         </Routes>
       </BrowserRouter>
