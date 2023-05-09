2154df09ae9771972e3cb843a0da1ab8f6f080ac
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-05-09 14:51:06 +0700

    ?? Section api

--- server/src/v1/routes/index.js
+++ server/src/v1/routes/index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var authRouter = require("./auth");
 var boardRouter = require("./board");
+var sectionRouter = require("./section");
 
 var router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/boards", boardRouter);
+router.use("/boards/:boardId/sections", sectionRouter);
 
 module.exports = router;
